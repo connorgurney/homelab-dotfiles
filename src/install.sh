6f6722b5ed20eb316f52b2ac88a2f202729d4ee4
Connor Gurney <me@connorgurney.me.uk>
2024-10-12 22:36:24 +0100

    Create SSH configuration

--- src/install.sh
+++ src/install.sh
@@ -4,3 +4,6 @@ REPO=$(pwd)
 # Symlink Git configuration
 ln -sf $REPO/src/dotfiles/.gitconfig ~/.gitconfig
 ln -sf $REPO/src/dotfiles/.gitignore ~/.gitignore
+
+# Symlink SSH configuration
+ln -sf $REPO/src/dotfiles/ssh/config ~/.ssh/config 
